c5842abc99fd2d711f3fa8d6bf7d2232b0415034
tashatemmy <temmydiva21@gmail.com>
2026-09-17 13:58:48 -0400

    Add CLI user login and initial input

--- main.py
+++ main.py
@@ -7,3 +7,11 @@ Author: Temi Odey, Python programmer
 Program Scope: inventory management, update cart total, receipt generation.
 
 """
+
+login_details = input("Dear user, enter your username to login to your account: ").strip().
+if login_details in  ["Temi","Titi","Tayo"]:
+	print(f"Welcome {login_details}, to CampusCart")
+else:
+	print("you need to signup to use campuscart")
+user_option = int(input("Select an option: ").strip())
+print(f"{login_details}, you selected option: {user_option}")
