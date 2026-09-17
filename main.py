524144dc025b68091f2f057e2178733681566e23
tashatemmy <temmydiva21@gmail.com>
2026-09-17 00:46:13 -0400

    descriptive docstring added to main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+"""
+CampusCart - Campus Vendor Command Line Interface Application
+
+An application designed to help campus vendors manage inventory, process and update shopping cart totals and generate sale receipts.
+
+Author: Temi Odey, Python programmer
+Program Scope: inventory management, update cart total, receipt generation.
+
+"""
